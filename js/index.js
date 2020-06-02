77db7864d0f3b9a67b29ceb65f1bc2bf5a0bb20c
Collin Ballou <BlueEagle@users.noreply.github.com>
2020-06-02 13:43:21 -0600

    added two event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+const funBusImage = document.querySelector('header img');
+
+funBusImage.addEventListener('mouseover', e => {
+  funBusImage.classList.add('blueShadow');
+})
+
+funBusImage.addEventListener('mouseleave', e => {
+  funBusImage.classList.remove('blueShadow');
+})
