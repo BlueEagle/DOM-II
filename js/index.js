23204d12ca0fb03e77a7f02bdaa63faccb06fe64
Collin Ballou <BlueEagle@users.noreply.github.com>
2020-06-02 14:09:39 -0600

    5 listeners implemented

--- js/index.js
+++ js/index.js
@@ -1,10 +1,29 @@
 // Your code goes here
 const funBusImage = document.querySelector('header img');
+const imgs = document.querySelectorAll('img');
 
-funBusImage.addEventListener('mouseover', e => {
-  funBusImage.classList.add('blueShadow');
+
+
+document.addEventListener('keydown', e => {
+  alert('Stop pressing my buttons!');
 })
 
-funBusImage.addEventListener('mouseleave', e => {
-  funBusImage.classList.remove('blueShadow');
+document.addEventListener('select', e => {
+  console.log(e);
+})
+
+imgs.forEach(img => {
+  img.addEventListener('mouseover', e => {
+    img.classList.add('blueShadow');
+  })
+  
+  img.addEventListener('mouseleave', e => {
+    img.classList.remove('blueShadow');
+  })
+
+  img.addEventListener('wheel', e => {
+    // debugger
+    // alert(`It took you ${e.timeStamp}ms to find this feature.`);
+    img.classList.toggle('fullRotate');
+  })
 })
