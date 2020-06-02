f9747d55191bdca29804f8e603bda53e98a04334
Collin Ballou <BlueEagle@users.noreply.github.com>
2020-06-02 16:25:41 -0600

    finished MVP

--- js/index.js
+++ js/index.js
@@ -7,13 +7,43 @@ window.addEventListener('load', e => {
   console.log('The page has loaded sucessfully!')
 })
 
+window.addEventListener('resize', e => {
+  const genRandHex = () => {
+    let num = Math.round(Math.random()*15)
+    if (num === 10) return 'a'
+    if (num === 11) return 'b'
+    if (num === 12) return 'c'
+    if (num === 13) return 'd'
+    if (num === 14) return 'e'
+    if (num === 15) return 'f'
+    return num
+  }
+  let r = genRandHex();
+  let g = genRandHex();
+  let b = genRandHex();
+  console.log(`${r}${g}${b}`);
+  body.style.backgroundColor = `#${r}${g}${b}`
+})
+
 document.addEventListener('keydown', e => {
   alert('Stop pressing my buttons!');
 })
 
 document.addEventListener('select', e => {
-  // let selected = e.target.value.substring(e.target.selectionStart, e.target.selectionEnd);
   alert(e);
+})
+
+let blue = false
+body.addEventListener('dblclick', e => {
+  blue = !blue
+  if (blue) body.style.backgroundColor = 'skyblue';
+  else body.style.backgroundColor = 'white';
+})
+
+funBusImage.addEventListener('contextmenu', e => {
+  e.preventDefault();
+  funBusImage.title = "You've activated the hover mode!";
+  console.log(funBusImage.title);
 })
 
 imgs.forEach(img => {
@@ -35,11 +65,7 @@ imgs.forEach(img => {
   })
 
   img.addEventListener('dblclick', e => {
+    e.stopPropagation();
     open(e.target.src)
   })
 })
-
-// 2 more to go!
-
-// double clicking text allows you to edit it
-// hitting enter saves it
